6cae75b08f5faf0578dc8fb37770a955fe8192b2
ebaneban888xyz <ebaneban888+github@gmail.com>
2026-09-01 13:51:14 +0800

    Remove logo image from form generator page

--- ruoyi-ui/src/views/tool/build/index.vue
+++ ruoyi-ui/src/views/tool/build/index.vue
@@ -3,7 +3,7 @@
     <div class="left-board">
       <div class="logo-wrapper">
         <div class="logo">
-          <img :src="logo" alt="logo"> Form Generator
+          Form Generator
         </div>
       </div>
       <el-scrollbar class="left-scrollbar">
@@ -147,7 +147,6 @@ import { makeUpHtml, vueTemplate, vueScript, cssStyle } from '@/utils/generator/
 import { makeUpJs } from '@/utils/generator/js'
 import { makeUpCss } from '@/utils/generator/css'
 import { drawingDefaultValue, initDrawingDefaultValue, cleanDrawingDefaultValue } from '@/utils/generator/drawingDefault'
-import logo from '@/assets/logo/logo.png'
 import CodeTypeDialog from './CodeTypeDialog'
 import DraggableItem from './DraggableItem'
 
@@ -165,7 +164,6 @@ export default {
   },
   data() {
     return {
-      logo,
       idGlobal: 100,
       formConf,
       inputComponents,
@@ -617,11 +615,6 @@ $lighterBlue: #409EFF;
   font-weight: 600;
   font-size: 17px;
   white-space: nowrap;
-  > img{
-    width: 30px;
-    height: 30px;
-    vertical-align: top;
-  }
   .github{
     display: inline-block;
     vertical-align: sub;
